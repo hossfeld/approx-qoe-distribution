b0b473342bae7b308e56e743169fee94685ff671
toh60bh <tobias.hossfeld@uni-wuerzburg.de>
2020-06-19 21:43:59 +0200

    QoE Management 2020 - presentation uploaded

---
+++ presentation/data/slide8.js
@@ -0,0 +1,13 @@
+(function(){var loadHandler=window['sl_{7A247DAF-0405-4DEC-9DF2-B4B212E797C1}'];loadHandler&&loadHandler(7, '<div id="spr0_1bdb793"><div id="spr1_1bdb793" class="kern slide"><img id="img2_1bdb793" src="data/img7.png" width="1040px" height="720px" alt="" style="left:0px;top:0px;"/><div id="spr3_1bdb793" style="left:980.87px;top:670.578px;"><div id="spr4_1bdb793"><img id="img0_1bdb793" src="data/img2.svg#svgView(preserveAspectRatio(none);)" width="35.011" height="35.011" alt=""/></div></div><div id="svg3_1bdb793" style="left:11.986px;top:69.667px;"><svg width="1014" height="6" viewBox="-3 -1.5 1014 6"><g fill="#e7e6e6" stroke="none"><path stroke="#e7e6e6" stroke-width="3" stroke-linecap="butt" transform="translate(-14.986145,-71.166664)" d="M14.986,72.667 H1022.305"/></g></svg></div></div><div id="spr2_1bdb793" class="kern slide"><div id="spr5_1bdb793" style="left:61.389px;top:3.167px;"><div style="width:0px;"><span id="txt0_1bdb793" class="nokern" data-width="178.875000" style="left:365.215px;top:11.34px;">Conclusions</span></div></div><div id="spr6_1bdb793" style="left:32.25px;top:106.5px;"><img id="img1_1bdb793" src="data/img46.png" width="831" height="487.5" alt="Beta distribution approximation\
+Methodology for approximating user rating distributions\
+given a QoS-to-MOS mapping \
+given second order statistic, SOS parameter 𝜃 for service\
+Discretization to discrete rating distributions (e.g., 1,2,3,4,5)\
+\
+Allows deriving QoE distributions in systems\
+Application to analytical or simulative QoS results for systems\
+In practice: based on (potentially correlated) QoS measurements\
+Allows: investigating impact of user diversity (𝜃) on QoE results\
+\
+Python scripts available at GitHub \
+https://github.com/hossfeld/approx-qoe-distribution"/></div><div id="spr7_1bdb793" style="left:61.389px;top:668.917px;"><div style="width:0px;"><span id="txt1_1bdb793" style="left:892.6px;top:10.379px;">8</span></div></div></div></div>', '{"s":[]}');})();
